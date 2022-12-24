0f7366fdca9368455008e5e315c1e429a8fc5b7b
ahmet <temiralpin@gmail.com>
2022-12-24 19:29:18 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,a,k,kullanicigirdi;
+    for (i=0; i<40; i++)
+    {
+        
+        for(j=40; j>a;j--)
+        {
+             printf(" ");
+           
+            
+        }
+        a++;
+         
+         
+        for (k=0; k<i; k++)
+        {
+            
+             printf("*");
+            
+        }
+         
+        
+          printf("\n");
+    }
+}
